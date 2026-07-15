28e75ac83e172624959aed2c1312a4e871ffaf87
DoceAzedo <me@doceazedo.com>
2026-07-15 13:30:31 -0300

    fix /me map

--- src/routes/me/+page.server.ts
+++ src/routes/me/+page.server.ts
@@ -1,8 +1,21 @@
-export const load = async ({ request }) => {
-	const lon = request.headers.get("x-vercel-ip-longitude");
-	const lat = request.headers.get("x-vercel-ip-latitude");
-	const location: [number, number] | null =
-		lon && lat ? [parseFloat(lon), parseFloat(lat)] : null;
+export const load = async ({ request, getClientAddress, fetch }) => {
+	const ip = request.headers.get("X-Real-Ip") || getClientAddress();
+	console.log(`===== IP: ${ip} =====`);
+
+	let lon = 0;
+	let lat = 0;
+
+	try {
+		const resp = await fetch("https://json.geoiplookup.io");
+		const data: { latitude: number; longitude: number } = await resp.json();
+		if (isNaN(data.longitude) || isNaN(data.latitude)) throw Error();
+		lon = data.longitude;
+		lat = data.latitude;
+	} catch (_error) {
+		/* empty */
+	}
+
+	const location: [number, number] | null = lon && lat ? [lon, lat] : null;
 	return {
 		location,
 	};

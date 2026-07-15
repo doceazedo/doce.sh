87952eaf46854c33bd2028ff91a3f491451d860f
DoceAzedo <me@doceazedo.com>
2026-07-15 14:02:02 -0300

    fix geoip lookup

--- src/routes/me/+page.server.ts
+++ src/routes/me/+page.server.ts
@@ -1,12 +1,11 @@
 export const load = async ({ request, getClientAddress, fetch }) => {
-	const ip = request.headers.get("X-Real-Ip") || getClientAddress();
-	console.log(`===== IP: ${ip} =====`);
+	const ip = request.headers.get("X-Real-Ip") || getClientAddress() || "";
 
 	let lon = 0;
 	let lat = 0;
 
 	try {
-		const resp = await fetch("https://json.geoiplookup.io");
+		const resp = await fetch(`https://json.geoiplookup.io/${ip}`);
 		const data: { latitude: number; longitude: number } = await resp.json();
 		if (isNaN(data.longitude) || isNaN(data.latitude)) throw Error();
 		lon = data.longitude;
